a154c02a5ca96fe05909640157deba737e782447
Adarsh <adarshtrip2306@gmail.com>
2025-02-03 23:01:34 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import NavHeader from "./components/Header";
 import HomePage from "./components/Home";
+import MainSection from "./components/main";
 
 function App() {
 	return (
 		<>
-			<div className="flex flex-col  ">
+			<div className="flex flex-col">
 				<NavHeader />
 				<HomePage />
+				<MainSection />
 			</div>
 		</>
 	);
---
+++ src/components/main.jsx
@@ -0,0 +1,27 @@
+export default function MainSection() {
+	function handleClick() {
+		alert("mf i have been clicked");
+	}
+
+	return (
+		<main>
+			<div className="flex justify-center item-center m-5">
+				<form className="flex gap-6 min-w-[150px]">
+					<input
+						type="text"
+						placeholder="eg. Sahi Paneer"
+						aria-label="Add indegredients"
+						className="border border-gray-300 p-2 rounded-md w-[50rem]"
+					/>
+					<button
+						onClick={handleClick}
+						type="submit"
+						className=" w-[30rem] bg-black text-white p-2 rounded-md hover:bg-green-600"
+					>
+						Add
+					</button>
+				</form>
+			</div>
+		</main>
+	);
+}
