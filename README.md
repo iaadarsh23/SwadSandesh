da380bcd56680a0bc4233be4313f9ef91217df57
Adarsh <adarshtrip2306@gmail.com>
2025-02-06 23:32:15 +0530

    updated

---
+++ README.md
@@ -0,0 +1,6 @@
+#things i have learned here and used-
+
+1. States in react- a build in component that allows the components to manage the data that can vary or change over time
+2. whenever the data changes state will rerender the component to reflect the updated data.
+3. usg ex- const [count, setCount]= React.useState('0');
+4. always use- we should use the call back function inside the state setter function when we want the new value by using the old value, it will behve in right manner;
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,20 +1,19 @@
 export default function MainSection() {
-	function handleClick() {
-		alert("mf i have been clicked");
-	}
+	function handleAdd() {}
 
 	return (
 		<main>
 			<div className="flex justify-center item-center m-5">
-				<form className="flex gap-6 min-w-[150px]">
+				<form className="flex gap-6 min-w-[150px] ">
 					<input
 						type="text"
 						placeholder="eg. Sahi Paneer"
 						aria-label="Add indegredients"
 						className="border border-gray-300 p-2 rounded-md w-[50rem]"
+						name="ingredients"
 					/>
 					<button
-						onClick={handleClick}
+						onClick={handleAdd}
 						type="submit"
 						className=" w-[30rem] bg-black text-white p-2 rounded-md hover:bg-green-600"
 					>
